6165b010ccbfc98f62cb65d067e350fcaa74b107
Muhammed <muhammedmoussa@hotmail.com>
2020-07-11 23:48:09 +0200

    add: users function

---
+++ functions/users.js
@@ -0,0 +1,29 @@
+import faunadb from 'faunadb'
+
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET
+})
+
+exports.handler = (event, context, callback) => {
+    return client.query(q.Paginate(q.Match(q.Ref("indexes/all_users"))))
+    .then((response) => {
+      const todoRefs = response.data
+      const getAllUsersDataQuery = todoRefs.map((ref) => {
+        return q.Get(ref)
+      })
+      
+      return client.query(getAllUsersDataQuery).then((ret) => {
+        return callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(ret)
+        })
+      })
+    }).catch((error) => {
+      console.log("error", error)
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      })
+    })
+  }
